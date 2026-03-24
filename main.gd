5ceddb3a53361c8ec796167a819cf9960f275e94
Alexander Gallant <superalexander@gmail.com>
2026-03-24 09:53:55 -0400

    Added Steps 1 through 4 of character creation to the UI

---
+++ main.gd
@@ -0,0 +1,15 @@
+extends TabContainer
+
+var base_takebacks := 0
+var takebacks_used := 0
+var base_group_takebacks := 0
+var group_takebacks_used := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
